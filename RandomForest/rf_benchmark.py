89eb7c1b31482040bdbcab0be2da9396c8f383d3
vikasdumca <vikas.dumca2012@gmail.com>
2015-08-19 12:04:32 +0530

    minor chnge

--- RandomForest/rf_benchmark.py
+++ RandomForest/rf_benchmark.py
@@ -16,11 +16,6 @@ def calcShannonEnt(dataSet):
         shannonEnt -= prob * log(prob, 2) # log base 2
     return shannonEnt
 
-##
-#   parameters are dataSet , columnNo and value of the column
-#
-##
-
 def splitDataSet(dataSet, axis, value):
     retDataSet = []
     for featVec in dataSet:
@@ -30,34 +25,26 @@ def splitDataSet(dataSet, axis, value):
             retDataSet.append(reducedFeatVec)
     return retDataSet
 
-##
-#   parameters are dataSet
-#
-##
 
 def chooseBestFeatureToSplit(dataSet):
     print 'best fit..'
-    numFeatures = len(dataSet[0]) - 1      #the last column is used for the labels
+    numFeatures = len(dataSet[0]) - 1      
     baseEntropy = calcShannonEnt(dataSet)
     bestInfoGain = 0.0; bestFeature = -1
-    for i in range(numFeatures):        #iterate over all the features
-        featList = [example[i] for example in dataSet]#create a list of all the examples of this feature
-        uniqueVals = set(featList)       #get a set of unique values
+    for i in range(numFeatures):        
+        featList = [example[i] for example in dataSet]
+        uniqueVals = set(featList)       
         newEntropy = 0.0
         for value in uniqueVals:
             subDataSet = splitDataSet(dataSet, i, value)
             prob = len(subDataSet)/float(len(dataSet))
             newEntropy += prob * calcShannonEnt(subDataSet)
-        infoGain = baseEntropy - newEntropy     #calculate the info gain; ie reduction in entropy
-        if (infoGain > bestInfoGain):       #compare this to the best gain so far
-            bestInfoGain = infoGain         #if better than current best, set to best
+        infoGain = baseEntropy - newEntropy     
+        if (infoGain > bestInfoGain):       
+            bestInfoGain = infoGain         
             bestFeature = i
-    return bestFeature                      #returns an integer
+    return bestFeature                      
 
-##
-#   parameter is one list of dataset
-#
-##
 
 def majorityCnt(classList):
     classCount={}
@@ -67,17 +54,13 @@ def majorityCnt(classList):
     sortedClassCount = sorted(classCount.iteritems(), key=operator.itemgetter(1), reverse=True)
     return sortedClassCount[0][0]
 
-##
-#   parameter is one list of dataset
-#
-##
 def createTree(dataSet,labels):
     classList = [example[-1] for example in dataSet]
 
 
     if classList.count(classList[0]) == len(classList):
-        return classList[0]#stop splitting when all of the classes are equal
-    if len(dataSet[0]) == 1: #stop splitting when there are no more features in dataSet
+        return classList[0]
+    if len(dataSet[0]) == 1: 
         return majorityCnt(classList)
 
 
@@ -93,7 +76,7 @@ def createTree(dataSet,labels):
 
     uniqueVals = set(featValues)
     for value in uniqueVals:
-        subLabels = labels[:]       #copy all of labels, so trees don't mess up existing labels
+        subLabels = labels[:]       
         myTree[bestFeatLabel][value] = createTree(splitDataSet(dataSet, bestFeat, value),subLabels)
     return myTree
 
